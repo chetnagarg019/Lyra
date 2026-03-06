bd3cea01d48b22749e205dd654a9eaac7c82f702
chetna <chetnagarg019@gmail.com>
2026-03-06 19:47:58 +0530

    commit #28

--- Backend/server.js
+++ Backend/server.js
@@ -1,3 +1,66 @@
+// import dotenv from "dotenv";
+// dotenv.config();
+// import express from "express";
+// import cors from "cors";
+// import connectDB from "./config/db.js";
+// import authRoutes from "./routes/authRoutes.js";
+// import musicRoutes from "./routes/musicRoutes.js";
+// import cookieParser from "cookie-parser";
+// // import services from "./services/storageServices.js"
+
+// connectDB();
+// const app = express();
+// //   app.use(cors({
+// //   origin: [
+// //     "http://localhost:5173",
+// //     "https://lyra-dusky.vercel.app"
+// //   ],
+// //   credentials: true,
+// // }));
+// // const allowedOrigins = [
+// //   "http://localhost:5173",
+// //   "https://lyra-dusky.vercel.app",
+// // ];
+
+// const allowedOrigins = [
+//   "http://localhost:5173",
+//   "https://lyra-dusky.vercel.app",
+//   /\.vercel\.app$/,
+// ];
+
+// app.use(
+//   cors({
+//     origin: function (origin, callback) {
+//       // allow requests with no origin (like mobile 
+// apps, curl)
+//       if (!origin || allowedOrigins.includes(origin)) {
+//         callback(null, true);
+//       } else {
+//         callback(new Error("Not allowed by CORS"));
+//       }
+//     },
+//     credentials: true,
+//   }),
+// );
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+// app.use(cookieParser());
+
+// app.get("/", (req, res) => {
+//   res.send("Server connected");
+// });
+
+// app.use("/api/auth", authRoutes);
+// app.use("/api/music", musicRoutes);
+
+// // ..port or server slot
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => {
+//   console.log(`Server is running on ${PORT}`);
+// }); //
+
+// //imagekit ka use krne ke liye or uska code likhne ke liye services/storageservices.js name ki file create hui
+
 import dotenv from "dotenv";
 dotenv.config();
 
